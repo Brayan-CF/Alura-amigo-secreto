0eb4e9a98a5009e8b07498d2a3c740bb967a12f1
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2025-01-27 23:34:57 -0400

    primeros pasos de prueba

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+alert('hola prueba')
+let nombre = 'Brayan'
+let edad = 15
+let numeroVentas = 40
+let saldoDisponible = 100
+let mensajeError = ('Ingrese algo ni puede estar vacio')
+alert(mensajeError)
