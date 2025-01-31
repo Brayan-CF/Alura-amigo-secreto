faf1fe4fef0c7b68dfde7c717f865bf613090e39
pequebrayan <eate.brayanjose.calderon.fe@unifranz.edu.bo>
2025-01-30 23:00:04 -0400

    implementando funcion que recorra el array amigos

--- app.js
+++ app.js
@@ -6,9 +6,27 @@
 let amigos = []
 
 // Implementacion de funcion para: agregar amigos.
-function AgregarAmigo() {
+function agregarAmigo() {
+  amigos.push(document.getElementById('amigo').value);
 
-amigos.push(document.getElementById('amigo').value);
-document.getElementById('amigo').value = "";
+  document.getElementById('amigo').value = "";
+  mostrarAmigos();
+}
+
+/* Implementacion de función que recorra el array amigos y agregue cada nombre como un
+elemento <li>
+*/
+function mostrarAmigos() {
+  let lista = document.getElementById('ListaAmigos');
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++){
+      //Crear un elemento <li> y asignarle el nombre del amigo en la posicion i
+      let elemento = document.createElement('li');
+      elemento.textContent = amigos[i];
+      //Agregar el elemento <li> a la lista
+      lista.appendChild(elemento);
+  }
 }
 
